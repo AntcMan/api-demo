1a82d059ae756bed21f4af3ebcbab336acd28ed4
Anthony Marion <anthonycmarion@gmail.com>
2023-05-21 17:09:34 +1000

    Articles controller updated with RESTful methods
    Articles controller has all necessary actions to undertake CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,12 @@ class ArticlesController < ApplicationController
   # GET /articles or /articles.json
   def index
     @articles = Article.all
+    render json: @articles
   end
 
   # GET /articles/1 or /articles/1.json
   def show
+    render json: @article
   end
 
   # GET /articles/new
@@ -15,50 +17,39 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  # POST /articles or /articles.json
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
   # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles or /articles.json
-  def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy
-
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    # THE 'SET_ARTICLE' METHOD FETCHES THE APPROPRIATE ARTICLE FROM THE DB BEFORE
+    # SELECTED RESTFUL ACTIONS CAN OCCUR
     def set_article
       @article = Article.find(params[:id])
     end
