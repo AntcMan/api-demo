ee29f66a627f36cb7f98f98283b2e305aeb50e1c
Anthony Marion <anthonycmarion@gmail.com>
2023-05-23 16:39:56 +1000

    Controller filed updated to remove CSRF token validation
    User can now create and delete articles without needing validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: %i[ create destroy ]
   before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles or /articles.json
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Article.create(title: "First article", body: "This is the body for my first article")
+Article.create(title: "Second article", body: "This is the body for my second article")
+Article.create(title: "Third article", body: "This is the body for my third article")
+Article.create(title: "Fourth article", body: "This is the body for my fourth article")
+Article.create(title: "Fifth article", body: "This is the body for my fifth article")
